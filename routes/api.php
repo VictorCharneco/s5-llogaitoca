6ba3d94366647ba2a0f889b8edebbc7cd1c7c870
Victor C.B. <victor.charneco.barba@gmail.com>
2026-02-04 16:16:23 +0100

    add users routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -10,4 +11,9 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:api')->group(function(){
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
+    
+    Route::middleware('admin')->group(function(){
+        Route::get('/users', [UserController::class, 'index']);
+        Route::get('/users/{id}', [UserController::class, 'show']);
+    });
 });
