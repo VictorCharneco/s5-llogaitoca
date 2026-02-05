42fbc1614cd3ad878927187e4b5699379dc3a8b7
Victor C.B. <victor.charneco.barba@gmail.com>
2026-02-05 11:09:47 +0100

    add instruments routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\InstrumentController;
 use App\Http\Controllers\Api\UserController;
 
 
@@ -15,5 +16,11 @@ Route::middleware('auth:api')->group(function(){
     Route::middleware('admin')->group(function(){
         Route::get('/users', [UserController::class, 'index']);
         Route::get('/users/{id}', [UserController::class, 'show']);
+        Route::post('/instruments', [InstrumentController::class, 'store']);
+        Route::put('/instruments/{id}', [InstrumentController::class, 'update']);
+        Route::delete('/instruments/{id}', [InstrumentController::class, 'destroy']);
     });
+
+    Route::get('/instruments', [InstrumentController::class, 'index']);
+    Route::get('/instruments/{id}', [InstrumentController::class, 'show']);
 });
