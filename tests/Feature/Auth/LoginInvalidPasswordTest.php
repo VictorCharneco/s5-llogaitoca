34b3bb4f584d3371016f7268e55d1640319c1ab2
Victor C.B. <victor.charneco.barba@gmail.com>
2026-02-12 15:56:09 +0100

    login with wrong password test

---
+++ tests/Feature/Auth/LoginInvalidPasswordTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Passport\ClientRepository;
+use Tests\TestCase;
+
+class LoginInvalidPasswordTest extends TestCase{
+    use RefreshDatabase;
+
+    public function test_login_rejected_with_wrong_password(): void{
+        app(ClientRepository::class)->createPersonalAccessGrantClient('Test Personal Access Client');
+
+        User::factory()->create([
+            'email' => 'test@example',
+            'password' => Hash::make('valido123'),
+            'role' => 'user',
+        ]);
+
+        $data = [
+            'email' => 'test@example.com',
+            'password' => 'erroneo321',
+        ];
+
+        $response = $this->postJson('api/login', $data);
+        $response->assertStatus(422)->assertJsonValidationErrors(['email']);
+    }    
+
+}
